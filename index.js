48666131b11490425ca0b2c695895ab494b47d66
ArmandoxxXD <mamojose58@gmail.com>
2023-12-04 15:48:58 -0600

    escuchar y emitir usuarios conectados

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const express = require("express");
 const { Server } = require("socket.io");
 const cors = require("cors");
 
-// Crear una aplicación Express y un servidor HTTP
 const app = express();
 app.use(cors());
 const server = http.createServer(app);
@@ -16,10 +15,10 @@ const io = new Server(server, {
   },
 });
 
+
 // Clase del Alert (Mediator)
 class AlertMediator {
-  constructor(io) {
-    this.io = io;
+  constructor() {
     this.users = [];
     this.receptores = [];
   }
@@ -29,80 +28,109 @@ class AlertMediator {
   }
 
   // Método para enviar alertas de reporte cargado
-  sendReporteCargado(sender,data) {
+  sendReporteCargado(idSender,sender,data) {
+    this.receptores=[]
     this.users.forEach((user) => {
-      if (user !== sender) {
+      if (user.username !== sender) {
         this.receptores.push(user)
+        const mensajeParaReceptor = `${'asdas' +sender, data}`;
+        io.to(user.id).emit("reporte-cargado", mensajeParaReceptor);
       }
     });
-    let uniqueSet = new Set(this.receptores);
-    const receivers = [...uniqueSet]
 
-    const newData = `${sender}, ${data}, y se le notifico a los usuarios: ${receivers}`;
-    this.io.emit("reporte-cargado", newData);
+    const nombresReceptores = this.receptores.map(user => user.username).join(", ");
+
+    let newData = '';
+    if (nombresReceptores.length !== 0) {
+      newData = `${sender}, ${data}, y se le notifico a los usuarios: ${nombresReceptores}`;
+    } else {
+      newData = `${sender}, ${data}`;
+    }
+
+    io.emit("reporte-cargado", newData);
   }
 
   // Método para enviar alertas de reporte eliminado
-  sendReporteEliminado(sender,data) {
-    this.users.forEach((user) => {
-      if (user !== sender) {
-        this.receptores.push(user)
-        const mensajeParaReceptor = `Eres recibidor :)`;
-        this.io.to(user.socketId).emit("reporte-cargado", mensajeParaReceptor);
-      }
-    });
-    let uniqueSet = new Set(this.receptores);
-    const receivers = [...uniqueSet]
-
+  sendReporteEliminado(idSender,sender,data) {
+    this.receptores=[]
     const fecha = new Date(data.data.fecha);
     const formattedDate = `${fecha.getMonth() + 1}/${fecha.getFullYear()}`; // Los meses van de 0 a 11, así que añadimos +1
 
-    const newData = `${sender}, ${data.message}, ${formattedDate}, y se le notifico a los usuarios: ${receivers}`;
-    this.io.to(sender.socketId).emit("reporte-eliminado", newData);
+    this.users.forEach((user) => {
+      if (user.username !== sender) {
+        this.receptores.push(user)
+        console.log('Recibidor:' +`${user.username}`)
+        const id = user.id
+        console.log(typeof id)
+        const mensajeParaReceptor = `${sender}, ${data.message}, ${formattedDate}`;
+        io.to(id).emit("reporte-eliminado", mensajeParaReceptor);
+      }
+    });
+
+    const nombresReceptores = this.receptores.map(user => user.username).join(", ");
+    console.log('Emisor:' + sender)
+
+    let newData = '';
+    if (nombresReceptores.length !== 0) {
+      newData = `${sender}, ${data.message}, ${formattedDate}, y se le notifico a los usuarios: ${nombresReceptores}`;
+    } else {
+      newData = `${sender}, ${data.message}, ${formattedDate}`;
+    }
+    io.emit("reporte-eliminado", newData);
   }
 }
 
-const alertMediator = new AlertMediator(io);
+const alertMediator = new AlertMediator();
+const connectedUsers = new Set(); 
 
 // Configurar eventos de Socket.io
 io.on("connection", (socket) => {
 
   // Agregar usuario al Mediator cuando se conecta
-  socket.on("login", (data) => {
-    console.log('Usuario Conectado:'+data);
-    socket.user = data || { user_id: socket.id }; 
-
-    alertMediator.addUser(socket.user)
+  socket.on("login", (name) => {
+    socket.join(`${name}`); 
+   
+    const user = {
+      id: socket.id,
+      username: name
+    }
+   
+    console.log('Usuario Conectado:')
+    console.log(user)
+    alertMediator.addUser(user,socket)
+    connectedUsers.add(name);
+    
+    // Enviamos la lista de usuarios conectados a todos los clientes
+    io.emit("usuarios-conectados", Array.from(connectedUsers));
   });
 
-  socket.on("reporte-cargado", (data) => {
-    alertMediator.sendReporteCargado(socket.user,data);
+  socket.on("reporte-cargado", (id,sender,data) => {
+    alertMediator.sendReporteCargado(id,sender,data);
   });
 
-  socket.on("reporte-eliminado", (data) => {
-    alertMediator.sendReporteEliminado(socket.user,data);
+  socket.on("reporte-eliminado", (id,sender,data) => {
+    alertMediator.sendReporteEliminado(id,sender,data);
   });
 
   // Desconexión del usuario
   socket.on("disconnect", () => {
-  if(socket.user){
-    console.log("Usuario desconectado:", socket.user);
-  }
-
-    // Eliminar usuario del Mediator cuando se desconecta
-    const index = alertMediator.users.indexOf(socket.user);
+    const index = alertMediator.users.findIndex(user => user.id === socket.id);
     if (index !== -1) {
+      console.log("Usuario desconectado:", alertMediator.users[index].username);
+      connectedUsers.delete(alertMediator.users[index].username);
       alertMediator.users.splice(index, 1);
     }
+      // Enviamos la lista actualizada de usuarios conectados a todos los clientes
+    io.emit("usuarios-conectados", Array.from(connectedUsers));
   });
 });
 
-// Configurar una ruta de prueba
+
 app.get("/", (res) => {
   res.send("Servicio Alertas UP");
 });
 
-// Iniciar el servidor en un puerto específico
+
 const port = process.env.PORT || 2000;
 server.listen(port, () => {
   console.log(`Servicio escuchando en el puerto ${port}`);
